643a35460dd722d54c97d80f1a141cfa93e5213e
yuyutsumadra <bpin333@gmail.com>
2026-09-11 12:01:26 +0530

    exception handling and logging completed

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,18 @@
+from logging import Logger
+import os
+from datetime import datetime
+import logging
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y,%H,%M,%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO,
+)
